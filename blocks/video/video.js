6b12ab8d523963166db8ddb93f1021037cb1d771
Kratika Gupta <que82035@adobe.com>
2024-09-18 23:35:28 +0530

    video

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,19 +1,13 @@
-// blocks/video/video.js
-
 export default function decorate(block) {
-    // Create iframe element for the video
     const iframe = document.createElement('iframe');
-    
-    // Configure iframe settings
     iframe.src = 'https://drive.google.com/file/d/1G0XzL27Gx9R5UWo5kkrkVgWNV7-6G0K9/view'; // Replace with actual video URL or dynamic handling
     iframe.width = '560';
     iframe.height = '315';
     iframe.Border = '0';
     iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
     iframe.allowFullscreen = true;
-  
-    // Append iframe to the block
-    block.innerHTML = ''; // Clear any existing content
+    block.innerHTML = ''; 
     block.appendChild(iframe);
-  }
+}
   
+
