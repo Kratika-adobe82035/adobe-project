0fd785f715ca06b744ceda13a11c4622245e49a6
Kratika Gupta <que82035@adobe.com>
2024-09-18 09:43:23 +0530

    table dynamic

--- blocks/table-dynamic/table-dynamic.js
+++ blocks/table-dynamic/table-dynamic.js
@@ -1,20 +1,20 @@
-
-
-
 async function createTableHeader(table){
     let tr=document.createElement("tr");
+    let sno=document.createElement("th");sno.appendChild(document.createTextNode("sNo"));
     let artname=document.createElement("th");artname.appendChild(document.createTextNode("Artname"));
     let price=document.createElement("th");price.appendChild(document.createTextNode("Price"));
     let discountprice=document.createElement("th");discountprice.appendChild(document.createTextNode("Discountprice"));
     tr.append(artname);tr.append(price);tr.append(discountprice);
     table.append(tr);
 }
+
 async function createTableRow(table,row,i){
     let tr=document.createElement("tr");
+    let sno=document.createElement("td");sno.appendChild(document.createTextNode(i));
     let artname=document.createElement("td");artname.appendChild(document.createTextNode(row.Artname));
-    let price=document.createElement("td");continent.appendChild(document.createTextNode(row.Price));
-    let discountprice=document.createElement("td");capital.appendChild(document.createTextNode(row.Discountprice));
-   tr.append(artname);tr.append(price);tr.append(discountprice);
+    let price=document.createElement("td");price.appendChild(document.createTextNode(row.Price));
+    let discountprice=document.createElement("td");discountprice.appendChild(document.createTextNode(row.Discountprice));
+    tr.append(artname);tr.append(price);tr.append(discountprice);
     table.append(tr);
 }
 
@@ -22,9 +22,12 @@ async function createTableRow(table,row,i){
 async function createTable(jsonURL,val) {
 
     let  pathname = null;
+
     if(val){
         pathname=jsonURL;
-    }else{
+    }
+
+    else{
         pathname= new URL(jsonURL);
     }
     
@@ -47,7 +50,7 @@ async function createTable(jsonURL,val) {
 export default async function decorate(block) {
     const artgallerylist = block.querySelector('a[href$=".json"]');
     const parientDiv=document.createElement('div');
-    parientDiv.classList.add('contries-block');
+    parientDiv.classList.add('table-dynamic-block');
 
     if (artgallerylist) {
         parientDiv.append(await createTable(artgallerylist.href,null));
@@ -56,3 +59,7 @@ export default async function decorate(block) {
     }
   
   }
+
+
+ 
+  
